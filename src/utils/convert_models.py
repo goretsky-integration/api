0ccbfa67cf3d99a7e6c27683d25509d1643213ed
usbtypec1 <eldos.baktybekov@gmail.com>
2022-06-23 20:01:41 +0600

    🚧 GI-7 `UnitDeliveryStatistics` to `ExtendedUnitDeliveryStatistics`

--- src/utils/convert_models.py
+++ src/utils/convert_models.py
@@ -1,5 +1,15 @@
 import models
-from utils.calculations import calculate_revenue_delta_in_percents
+from utils.calculations import (
+    calculate_revenue_delta_in_percents,
+    calculate_couriers_workload,
+    calculate_delivery_with_courier_app_percent,
+    calculate_orders_for_courier_count_per_hour,
+)
+
+__all__ = (
+    'weekly_operational_statistics_to_revenue_statistics',
+    'extend_unit_delivery_statistics',
+)
 
 
 def weekly_operational_statistics_to_revenue_statistics(
@@ -12,5 +22,25 @@ def weekly_operational_statistics_to_revenue_statistics(
         delta_from_week_before=calculate_revenue_delta_in_percents(
             revenue_today=operational_statistics.today.revenue,
             revenue_week_before=operational_statistics.week_before.revenue,
+        )
+    )
+
+
+def extend_unit_delivery_statistics(
+        delivery_statistics: models.UnitDeliveryStatistics
+) -> models.UnitDeliveryStatisticsExtended:
+    return models.UnitDeliveryStatisticsExtended(
+        **delivery_statistics.dict(by_alias=True),
+        orders_for_courier_count_per_hour=calculate_orders_for_courier_count_per_hour(
+            delivery_statistics.delivery_orders_count,
+            delivery_statistics.couriers_shifts_duration,
+        ),
+        delivery_with_courier_app_percent=calculate_delivery_with_courier_app_percent(
+            delivery_statistics.orders_with_courier_app_count,
+            delivery_statistics.delivery_orders_count,
+        ),
+        couriers_workload=calculate_couriers_workload(
+            delivery_statistics.trips_duration,
+            delivery_statistics.couriers_shifts_duration,
         )
     )
