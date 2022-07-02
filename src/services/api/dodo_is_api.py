2d28d46164787221d3a6fd50f5cb6af7c1eaa23a
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-02 12:18:09 +0600

    :construction: Add API requests

--- src/services/api/dodo_is_api.py
+++ src/services/api/dodo_is_api.py
@@ -1,16 +1,20 @@
-from typing import Iterable
+import asyncio
+from typing import Iterable, TypeAlias
 
 import httpx
 import pandas as pd
 from fastapi import HTTPException, status
 
 import models
+from utils import exceptions
 from services import parsers
 
 __all__ = (
     'get_restaurant_orders',
     'get_kitchen_statistics',
 )
+
+KitchenStatisticsAPIResponse: TypeAlias = tuple[models.KitchenStatistics | exceptions.KitchenStatisticsError]
 
 
 async def get_restaurant_orders(
@@ -42,5 +46,23 @@ async def get_kitchen_statistics(
     async with httpx.AsyncClient(cookies=cookies) as client:
         response = await client.get(url, params=params, timeout=30)
         if not response.is_success:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+            raise exceptions.KitchenStatisticsError(unit_id=unit_id)
         return parsers.KitchenStatisticsParser(response.text, unit_id).parse()
+
+
+async def get_kitchen_statistics_batch(
+        cookies: dict,
+        unit_ids: Iterable[int | str],
+) -> models.KitchenStatisticsBatch:
+    tasks = (get_kitchen_statistics(cookies, unit_id) for unit_id in unit_ids)
+    responses: tuple[KitchenStatisticsAPIResponse, ...] = await asyncio.gather(*tasks, return_exceptions=True)
+    return models.KitchenStatisticsBatch(
+        kitchen_statistics=[
+            response for response in responses
+            if isinstance(response, models.KitchenStatistics)
+        ],
+        error_unit_ids=[
+            response for response in responses
+            if isinstance(response, exceptions.KitchenStatisticsError)
+        ]
+    )
--- src/services/api/private_dodo_api.py
+++ src/services/api/private_dodo_api.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 from typing import Iterable
 
 import httpx
@@ -6,26 +7,90 @@ from pydantic import parse_obj_as
 
 import models
 from core import config
-from utils import time_utils
+from utils import time_utils, exceptions
 
 
 async def get_delivery_statistics(
         token: str,
-        unit_uuids: Iterable[uuid.UUID]
+        unit_uuids: Iterable[uuid.UUID],
+        from_datetime: datetime | None = None,
+        to_datetime: datetime | None = None,
 ) -> list[models.UnitDeliveryStatistics]:
+    url = 'https://api.dodois.io/dodopizza/ru/delivery/statistics/'
+    response_json = await request_to_private_dodo_api(url, token, unit_uuids, from_datetime, to_datetime)
+    return parse_obj_as(list[models.UnitDeliveryStatistics],
+                        response_json['unitsStatistics'])
+
+
+async def get_ingredient_stop_sales(
+        token: str,
+        unit_uuids: Iterable[uuid.UUID],
+        from_datetime: datetime,
+        to_datetime: datetime,
+) -> list[models.StopSalesByIngredients]:
+    url = 'https://api.dodois.io/dodopizza/ru/production/stop-sales-ingredients'
+    ingredient_stop_sales = await request_to_private_dodo_api(url, token, unit_uuids, from_datetime, to_datetime)
+    return parse_obj_as(list[models.StopSalesByIngredients],
+                        ingredient_stop_sales['stopSalesByIngredients'])
+
+
+async def get_channels_stop_sales(
+        token: str,
+        unit_uuids: Iterable[uuid.UUID],
+        from_datetime: datetime,
+        to_datetime: datetime,
+) -> list[models.StopSalesBySalesChannels]:
+    url = 'https://api.dodois.io/dodopizza/ru/production/stop-sales-channels'
+    channels_stop_sales = await request_to_private_dodo_api(url, token, unit_uuids, from_datetime, to_datetime)
+    return parse_obj_as(list[models.StopSalesBySalesChannels],
+                        channels_stop_sales['stopSalesBySalesChannels'])
+
+
+async def get_products_stop_sales(
+        token: str,
+        unit_uuids: Iterable[uuid.UUID],
+        from_datetime: datetime,
+        to_datetime: datetime,
+) -> list[models.StopSalesByProduct]:
+    url = 'https://api.dodois.io/dodopizza/ru/production/stop-sales-products'
+    products_stop_sales = await request_to_private_dodo_api(url, token, unit_uuids, from_datetime, to_datetime)
+    return parse_obj_as(list[models.StopSalesByProduct],
+                        products_stop_sales['stopSalesByProduct'])
+
+
+async def get_production_statistics(
+        token: str,
+        unit_uuids: Iterable[uuid.UUID],
+        from_datetime: datetime | None = None,
+        to_datetime: datetime | None = None,
+) -> list[models.OrdersHandoverTime]:
+    url = 'https://api.dodois.io/dodopizza/ru/production/orders-handover-time'
+    response_json = await request_to_private_dodo_api(url, token, unit_uuids, from_datetime, to_datetime)
+    return parse_obj_as(list[models.OrdersHandoverTime], response_json['ordersHandoverTime'])
+
+
+async def request_to_private_dodo_api(
+        url: str,
+        token: str,
+        unit_uuids: Iterable[uuid.UUID],
+        from_datetime: datetime | None = None,
+        to_datetime: datetime | None = None,
+) -> dict:
+    if from_datetime is None:
+        from_datetime = time_utils.get_moscow_datetime_now()
+    if to_datetime is None:
+        to_datetime = time_utils.get_moscow_datetime_now()
     headers = {
         'User-Agent': config.APP_USER_AGENT,
         'Authorization': f'Bearer {token}',
     }
     params = {
         'units': ','.join([i.hex for i in unit_uuids]),
-        'from': time_utils.get_moscow_datetime_now().strftime('%Y-%m-%dT00:00:00'),
-        'to': time_utils.get_moscow_datetime_now().strftime('%Y-%m-%dT%H:%M:%S'),
+        'from': from_datetime.strftime('%Y-%m-%dT00:00:00'),
+        'to': to_datetime.strftime('%Y-%m-%dT%H:%M:%S'),
     }
-    url = 'https://api.dodois.io/dodopizza/ru/delivery/statistics/'
     async with httpx.AsyncClient() as client:
         response = await client.get(url, params=params, headers=headers)
     if not response.is_success:
-        raise
-    return parse_obj_as(list[models.UnitDeliveryStatistics],
-                        response.json()['unitsStatistics'])
+        raise exceptions.PrivateDodoAPIError(status_code=response.status_code)
+    return response.json()
