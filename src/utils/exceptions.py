c434e52f1d1904748d6858974a4a382ab748f1f6
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-02 11:56:53 +0600

    :construction: Add exception classes for API requests

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -2,6 +2,23 @@ class PublicDodoAPIError(Exception):
     pass
 
 
+class DodoISAPIError(Exception):
+    pass
+
+
+class PrivateDodoAPIError(Exception):
+
+    def __init__(self, *args, status_code: int, **kwargs):
+        self.status_code = status_code
+
+
+class KitchenStatisticsError(DodoISAPIError):
+
+    def __init__(self, *args, unit_id: int | str, **kwargs):
+        self.unit_id = unit_id
+        super(*args, **kwargs)
+
+
 class OperationalStatisticsAPIError(PublicDodoAPIError):
 
     def __init__(self, *args, unit_id: int | str, **kwargs):
