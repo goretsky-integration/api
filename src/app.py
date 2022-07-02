d1249cf5ab2260ca00ba60192aee7958997e4dd7
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-02 12:31:24 +0600

    :goal_net: Add exception handlers

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 import endpoints
+from utils import exceptions
 
 __all__ = (
     'app',
@@ -8,3 +10,9 @@ __all__ = (
 
 app = FastAPI()
 app.include_router(endpoints.statistics.router, prefix='/v1')
+app.include_router(endpoints.stop_sales.router, prefix='/v1')
+
+
+@app.exception_handler(exceptions.PrivateDodoAPIError)
+async def on_private_dodo_api_error(request, exc: exceptions.PrivateDodoAPIError):
+    return JSONResponse({'error': 'error'}, status_code=exc.status_code)
