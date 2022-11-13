a0e4e2d5bd73b4904457f77da64d8e18093d5650
Eldos <eldos.baktybekov@gmail.com>
2022-11-13 22:52:07 +0600

    :bug: Filter only restaurant orders

--- src/v2/services/production_statistics.py
+++ src/v2/services/production_statistics.py
@@ -20,8 +20,12 @@ def calculate_average_tracking_pending_and_cooking_time(
 ) -> int:
     average_tracking_pending_and_cooking_time = 0
     if orders:
-        average_cooking_time = statistics.mean([order.cooking_time for order in orders])
-        average_tracking_pending_time = statistics.mean([order.tracking_pending_time for order in orders])
+        average_cooking_time = statistics.mean(
+            [order.cooking_time for order in orders
+             if order.sales_channel.name == models.SalesChannel.DINE_IN.name])
+        average_tracking_pending_time = statistics.mean(
+            [order.tracking_pending_time for order in orders
+             if order.sales_channel.name == models.SalesChannel.DINE_IN.name])
         average_tracking_pending_and_cooking_time = average_cooking_time + average_tracking_pending_time
     return average_tracking_pending_and_cooking_time
 
