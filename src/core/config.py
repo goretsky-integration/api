57aee2e8d3ca745a13ecbb1690f90650ac9372dc
Eldos <eldos.baktybekov@gmail.com>
2023-01-15 12:42:31 +0600

    :wrench: GI-3 Remove unused 'USE_CACHE' setting

--- src/core/config.py
+++ src/core/config.py
@@ -20,7 +20,6 @@ class AppSettings(BaseSettings):
     host: str = Field(..., env='APP_HOST')
     is_debug: bool = Field(..., env='IS_DEBUG')
     redis_url: str = Field(..., env='REDIS_URL')
-    use_cache: bool = Field(..., env='USE_CACHE')
 
 
 app_settings = AppSettings()
