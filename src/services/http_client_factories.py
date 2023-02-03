23bd344348b1b8be9e3e5053159a9c712bb787ab
Eldos <eldos.baktybekov@gmail.com>
2023-02-03 09:26:03 +0600

    :construction: GI-9 Add shift manager api client factory

--- src/services/http_client_factories.py
+++ src/services/http_client_factories.py
@@ -2,6 +2,7 @@ import contextlib
 from typing import TypeAlias
 
 import httpx
+from fastapi import Request
 
 __all__ = (
     'closing_dodo_is_api_client_factory',
@@ -31,16 +32,26 @@ async def closing_public_api_client_factory(*, country_code: str, app_user_agent
 
 
 @contextlib.asynccontextmanager
-async def closing_office_manager_api_client_factory(*, country_code: str, app_user_agent: str) -> HTTPClient:
+async def closing_office_manager_api_client_factory(
+        *,
+        cookies: dict[str, str],
+        country_code: str,
+        app_user_agent: str,
+) -> HTTPClient:
     base_url = f'https://officemanager.dodopizza.{country_code}/'
     headers = {'User-Agent': app_user_agent}
-    async with httpx.AsyncClient(headers=headers, base_url=base_url, timeout=120) as client:
+    async with httpx.AsyncClient(headers=headers, cookies=cookies, base_url=base_url, timeout=120) as client:
         yield client
 
 
 @contextlib.asynccontextmanager
-async def closing_shift_manager_api_client_factory(*, country_code: str, app_user_agent: str) -> HTTPClient:
+async def closing_shift_manager_api_client_factory(
+        *,
+        cookies: dict[str, str],
+        country_code: str,
+        app_user_agent: str,
+) -> HTTPClient:
     base_url = f'https://shiftmanager.dodopizza.{country_code}/'
     headers = {'User-Agent': app_user_agent}
-    async with httpx.AsyncClient(headers=headers, base_url=base_url, timeout=120) as client:
+    async with httpx.AsyncClient(headers=headers, cookies=cookies, base_url=base_url, timeout=120) as client:
         yield client
