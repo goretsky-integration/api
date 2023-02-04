43e1377157ede9174659eb15fe593c97f2211c92
Eldos <eldos.baktybekov@gmail.com>
2023-02-04 22:44:43 +0600

    :recycle: GI-9 Refactor v1 orders schemas

--- src/v1/endpoints/orders.py
+++ src/v1/endpoints/orders.py
@@ -1,12 +1,14 @@
 import asyncio
+from typing import Iterable
 
 from fastapi import APIRouter, Body, Depends
 
 from services.external_dodo_api import OfficeManagerAPI, ShiftManagerAPI
 from services.http_client_factories import HTTPClient
 from services.periods import Period
+from v1.endpoints import schemas
 from v1.endpoints.dependencies import get_closing_office_manager_api_client, get_closing_shift_manager_api_client
-from v1.models import UnitIdsAndNamesIn, CheatedOrders, OrderByUUID
+from v1.models import UnitIdsAndNamesIn, CheatedOrders
 from v1.services.orders import restaurant_orders_to_cheated_orders
 
 router = APIRouter(prefix='/v1', tags=['Orders'])
@@ -20,7 +22,7 @@ async def get_cheated_orders(
         units: UnitIdsAndNamesIn = Body(),
         repeated_phone_number_count_threshold: int = Body(),
         closing_office_manager_api_client: HTTPClient = Depends(get_closing_office_manager_api_client),
-):
+) -> Iterable[schemas.CheatedOrders]:
     period = Period.today()
     unit_ids = [unit.id for unit in units]
     async with closing_office_manager_api_client as client:
@@ -31,11 +33,10 @@ async def get_cheated_orders(
 
 @router.get(
     path='/canceled-orders',
-    response_model=list[OrderByUUID],
 )
 async def get_canceled_orders(
         closing_shift_manager_api_client: HTTPClient = Depends(get_closing_shift_manager_api_client),
-):
+) -> Iterable[schemas.OrderByUUID]:
     period = Period.today()
     async with closing_shift_manager_api_client as client:
         tasks = []
--- src/v1/endpoints/schemas/__init__.py
+++ src/v1/endpoints/schemas/__init__.py
@@ -1,3 +1,4 @@
+from .orders import *
 from .reports import *
 from .stocks import *
 from .stop_sales import *
