88c61fa16a1056ce32e9d7df1c3eb3999e8fec35
Eldos <eldos.baktybekov@gmail.com>
2023-02-04 13:27:59 +0600

    :recycle: GI-9 Refactor reports

--- src/v1/endpoints/reports.py
+++ src/v1/endpoints/reports.py
@@ -1,23 +1,30 @@
 import asyncio
 import tempfile
 
-import httpx
 from fastapi import APIRouter, Query, Body, Depends
 from fastapi_cache.decorator import cache
 
-from core import config
 from services.http_client_factories import HTTPClient
 from v1 import exceptions, models
-from v1.endpoints.dependencies import get_closing_public_api_client
-from v1.models import RevenueStatisticsReport, UnitsRevenueStatistics, UnitIDsIn, \
-    DeliveryPartialStatisticsReport, UnitDeliveryPartialStatistics, KitchenPartialStatisticsReport, \
-    UnitKitchenPartialStatistics, UnitBonusSystemStatistics, UnitIdsAndNamesIn
+from v1.endpoints.dependencies import get_closing_public_api_client, get_closing_office_manager_api_client
+from v1.models import (
+    RevenueStatisticsReport,
+    UnitsRevenueStatistics,
+    UnitIDsIn,
+    DeliveryPartialStatisticsReport,
+    UnitDeliveryPartialStatistics,
+    KitchenPartialStatisticsReport,
+    UnitKitchenPartialStatistics,
+    UnitBonusSystemStatistics,
+    UnitIdsAndNamesIn,
+)
 from v1.parsers import DeliveryStatisticsExcelParser
-from v1.services import operational_statistics
-from v1.services.delivery import get_delivery_statistics_excel
-from v1.services.external_dodo_api import DodoPublicAPI, get_operational_statistics_for_today_and_week_before_batch
+from v1.services.external_dodo_api import (
+    DodoPublicAPI,
+    OfficeManagerAPI,
+    get_operational_statistics_for_today_and_week_before_batch,
+)
 from v1.services.operational_statistics import calculate_units_revenue, calculate_total_revenue
-from v1.services.orders import get_restaurant_orders
 from v2.periods import Period
 
 router = APIRouter(tags=['Reports'])
@@ -45,49 +52,58 @@ async def get_revenue_statistics(
     )
 
 
-@router.post(
+@router.get(
     path='/v1/reports/awaiting-orders',
     response_model=DeliveryPartialStatisticsReport,
 )
 @cache(expire=60, namespace='awaiting-orders')
-async def get_delivery_partial_statistics(unit_ids: UnitIDsIn, cookies: dict = Body()):
-    async with httpx.AsyncClient(cookies=cookies, headers={'User-Agent': config.APP_USER_AGENT}) as client:
-        tasks = (operational_statistics.get_delivery_partial_statistics(client, unit_id) for unit_id in unit_ids)
+async def get_delivery_partial_statistics(
+        unit_ids: UnitIDsIn = Query(),
+        closing_office_manager_api_client: HTTPClient = Depends(get_closing_office_manager_api_client),
+):
+    async with closing_office_manager_api_client as client:
+        api = OfficeManagerAPI(client)
+        tasks = (api.get_delivery_partial_statistics(unit_id) for unit_id in unit_ids)
         results = await asyncio.gather(*tasks, return_exceptions=True)
     delivery_partial_statistics = [result for result in results if isinstance(result, UnitDeliveryPartialStatistics)]
     errors = [result.unit_id for result in results if isinstance(result, exceptions.UnitIDAPIError)]
     return DeliveryPartialStatisticsReport(results=delivery_partial_statistics, errors=errors)
 
 
-@router.post(
+@router.get(
     path='/v1/reports/kitchen-productivity',
     response_model=KitchenPartialStatisticsReport,
 )
 @cache(expire=60, namespace='kitchen-productivity')
-async def get_kitchen_partial_statistics(unit_ids: UnitIDsIn, cookies: dict = Body()):
-    async with httpx.AsyncClient(cookies=cookies, headers={'User-Agent': config.APP_USER_AGENT}) as client:
-        tasks = (operational_statistics.get_kitchen_partial_statistics(client, unit_id) for unit_id in unit_ids)
+async def get_kitchen_partial_statistics(
+        unit_ids: UnitIDsIn = Query(),
+        closing_office_manager_api_client: HTTPClient = Depends(get_closing_office_manager_api_client),
+):
+    async with closing_office_manager_api_client as client:
+        api = OfficeManagerAPI(client)
+        tasks = (api.get_kitchen_partial_statistics(unit_id) for unit_id in unit_ids)
         results = await asyncio.gather(*tasks, return_exceptions=True)
     kitchen_partial_statistics = [result for result in results if isinstance(result, UnitKitchenPartialStatistics)]
     errors = [result.unit_id for result in results if isinstance(result, exceptions.UnitIDAPIError)]
     return KitchenPartialStatisticsReport(results=kitchen_partial_statistics, errors=errors)
 
 
-@router.post(
+@router.get(
     path='/v1/reports/bonus-system',
     response_model=list[UnitBonusSystemStatistics],
 )
 @cache(expire=60, namespace='bonus-system')
 async def get_bonus_system_statistics(
-        units: UnitIdsAndNamesIn = Body(),
-        cookies: dict = Body(),
+        unit_ids_and_names: UnitIdsAndNamesIn = Query(),
+        closing_office_manager_api_client: HTTPClient = Depends(get_closing_office_manager_api_client),
 ):
     period = Period.today()
-    unit_ids = {unit.id for unit in units}
-    unit_name_to_id = {unit.name: unit.id for unit in units}
-    all_orders = await get_restaurant_orders(cookies, unit_ids, period)
+    unit_ids = {unit.id for unit in unit_ids_and_names}
+    async with closing_office_manager_api_client as client:
+        api = OfficeManagerAPI(client)
+        all_orders = await api.get_restaurant_orders(unit_ids, period)
+    unit_name_to_id = {unit.name: unit.id for unit in unit_ids_and_names}
     existing_unit_ids = set()
-
     results = []
     for name, orders in all_orders:
         unit_id = unit_name_to_id[name]
@@ -115,10 +131,12 @@ async def get_bonus_system_statistics(
 )
 async def on_get_trips_with_one_order(
         unit_ids: set[int] = Body(),
-        cookies: dict = Body(),
+        closing_office_manager_api_client: HTTPClient = Depends(get_closing_office_manager_api_client),
 ):
-    with httpx.Client(cookies=cookies) as client:
-        delivery_statistics_excel = get_delivery_statistics_excel(client, unit_ids, Period.today())
+    period = Period.today()
+    async with closing_office_manager_api_client as client:
+        api = OfficeManagerAPI(client)
+        delivery_statistics_excel = await api.get_delivery_statistics_excel(unit_ids, period)
     with tempfile.NamedTemporaryFile(suffix='.xlsx') as temp_file:
         temp_file.write(delivery_statistics_excel)
         return DeliveryStatisticsExcelParser(temp_file.name).parse()
