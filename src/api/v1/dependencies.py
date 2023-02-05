1f53387f33817944df25e0ee7566a0c39daac3e2
Eldos <eldos.baktybekov@gmail.com>
2023-02-05 12:29:49 +0600

    :recycle: GI-9 Use country code schema in dependencies

--- src/api/v1/dependencies.py
+++ src/api/v1/dependencies.py
@@ -1,5 +1,6 @@
 from fastapi import Request
 
+from api import common_schemas
 from core import config
 from services.http_client_factories import closing_public_api_client_factory, closing_office_manager_api_client_factory, \
     HTTPClient, closing_shift_manager_api_client_factory
@@ -11,21 +12,21 @@ __all__ = (
 )
 
 
-def get_closing_public_api_client(*, country_code: str) -> HTTPClient:
-    return closing_public_api_client_factory(country_code=country_code, app_user_agent=config.APP_USER_AGENT)
+def get_closing_public_api_client(*, country_code: common_schemas.CountryCode) -> HTTPClient:
+    return closing_public_api_client_factory(country_code=country_code.value, app_user_agent=config.APP_USER_AGENT)
 
 
-def get_closing_office_manager_api_client(*, request: Request, country_code: str) -> HTTPClient:
+def get_closing_office_manager_api_client(*, request: Request, country_code: common_schemas.CountryCode) -> HTTPClient:
     return closing_office_manager_api_client_factory(
         cookies=request.cookies,
-        country_code=country_code,
+        country_code=country_code.value,
         app_user_agent=config.APP_USER_AGENT,
     )
 
 
-def get_closing_shift_manager_api_client(*, request: Request, country_code: str) -> HTTPClient:
+def get_closing_shift_manager_api_client(*, request: Request, country_code: common_schemas.CountryCode) -> HTTPClient:
     return closing_shift_manager_api_client_factory(
         cookies=request.cookies,
-        country_code=country_code,
+        country_code=country_code.value,
         app_user_agent=config.APP_USER_AGENT,
     )
--- src/api/v2/dependencies.py
+++ src/api/v2/dependencies.py
@@ -1,5 +1,6 @@
 from fastapi import Depends
 
+from api import common_schemas
 from core import config
 from services.http_client_factories import closing_dodo_is_api_client_factory
 from api.v2.bearer import AccessTokenBearer
@@ -7,11 +8,11 @@ from api.v2.bearer import AccessTokenBearer
 
 def get_closing_dodo_is_api_client(
         *,
-        country_code: str,
+        country_code: common_schemas.CountryCode,
         token: str = Depends(AccessTokenBearer()),
 ):
     return closing_dodo_is_api_client_factory(
-        country_code=country_code,
+        country_code=country_code.value,
         token=token,
         app_user_agent=config.APP_USER_AGENT,
     )
