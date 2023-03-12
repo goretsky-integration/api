6ebd98b18cc78b74ba7910dc79b845d435003eec
Eldos <eldos.baktybekov@gmail.com>
2023-03-12 10:42:22 +0600

    :construction: Add http client factory for export service (#GI-33)

--- src/services/http_client_factories.py
+++ src/services/http_client_factories.py
@@ -2,16 +2,18 @@ import contextlib
 from typing import TypeAlias
 
 import httpx
-from fastapi import Request
 
 __all__ = (
     'closing_dodo_is_api_client_factory',
     'closing_office_manager_api_client_factory',
     'closing_public_api_client_factory',
     'closing_shift_manager_api_client_factory',
+    'closing_export_service_api_client_factory',
     'AsyncHTTPClient',
+    'HTTPClient',
 )
 
+HTTPClient: TypeAlias = httpx.Client
 AsyncHTTPClient: TypeAlias = httpx.AsyncClient
 
 
@@ -54,4 +56,17 @@ async def closing_shift_manager_api_client_factory(
     base_url = f'https://shiftmanager.dodopizza.{country_code}/'
     headers = {'User-Agent': app_user_agent}
     async with httpx.AsyncClient(headers=headers, cookies=cookies, base_url=base_url, timeout=120) as client:
+        yield client
+
+
+@contextlib.contextmanager
+def closing_export_service_api_client_factory(
+        *,
+        cookies: dict[str, str],
+        country_code: str,
+        app_user_agent: str,
+) -> HTTPClient:
+    base_url = f'https://officemanager.dodopizza.{country_code}/'
+    headers = {'User-Agent': app_user_agent}
+    with httpx.Client(headers=headers, cookies=cookies, base_url=base_url, timeout=120) as client:
         yield client
