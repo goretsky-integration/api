8a5ecd200527ed0ce786aa1041b53f7cbad1799b
Eldos <eldos.baktybekov@gmail.com>
2023-03-12 10:43:51 +0600

    :construction: Add export service api (#GI-33)

---
+++ src/services/external_dodo_api/export_service.py
@@ -0,0 +1,34 @@
+from typing import Iterable
+
+import httpx
+
+from services.periods import Period
+
+
+class ExportServiceAPI:
+
+    def __init__(self, http_client: httpx.Client):
+        self.__http_client = http_client
+
+    def get_promo_codes_excel_report(self, period: Period, unit_ids: Iterable[int]) -> bytes:
+        url = '/Reports/PromoCodeUsed/Export'
+        request_data = {
+            'unitsIds': tuple(unit_ids),
+            'OrderSources': (
+                'Telephone',
+                'Site',
+                'Restaurant',
+                'DefectOrder',
+                'Mobile',
+                'Pizzeria',
+                'Aggregator',
+                'Kiosk',
+            ),
+            'beginDate': period.start.strftime('%d.%m.%Y'),
+            'endDate': period.end.strftime('%d.%m.%Y'),
+            'orderTypes': ('Delivery', 'Pickup', 'Stationary'),
+            'IsAllPromoCode': True,
+            'OnlyComposition': False,
+        }
+        response = self.__http_client.post(url, data=request_data)
+        return response.content
